e0e827ad60b91ef170d480aba86bc957b7362f6c
David Berenstein <david@flow.ai>
2020-09-13 15:13:12 +0200

    name formatting encoder and decoder

--- model/decoder.py
+++ model/decoder.py
@@ -1,7 +1,6 @@
 import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
-
 from utils.functional import parameters_allocation_check
 
 
@@ -18,14 +17,13 @@ class Decoder(nn.Module):
 
         self.fc = nn.Linear(self.params.decoder_rnn_size, self.params.word_vocab_size)
 
-
     def only_decoder_beam(self, decoder_input, z, drop_prob, initial_state=None):
-        
+
         assert parameters_allocation_check(self), \
             'Invalid CUDA options. Parameters should be allocated in the same memory'
 
 #         print decoder_input.size()
-         
+
         [beam_batch_size, _, _] = decoder_input.size()
 
         '''
@@ -34,30 +32,29 @@ class Decoder(nn.Module):
         decoder_input = F.dropout(decoder_input, drop_prob)
 
         z = z.unsqueeze(0)
-        
+
 #         print z.size()
 
         z = t.cat([z] * beam_batch_size, 0)
-        
+
 #         print z.size()
 #         z = z.contiguous().view(1, -1)
 
 #         z = z.view(beam_batch_size, self.params.latent_variable_size)
-        
-#         print z.size() 
-        
+
+#         print z.size()
+
         decoder_input = t.cat([decoder_input, z], 2)
 
-#         print "decoder_input:",decoder_input.size() 
+#         print "decoder_input:",decoder_input.size()
 
-        rnn_out, final_state = self.rnn(decoder_input, initial_state) 
+        rnn_out, final_state = self.rnn(decoder_input, initial_state)
 
 #         print "rnn_out:",rnn_out.size()
 #         print "final_state_1:",final_state[0].size()
-#         print "final_state_1:",final_state[1].size()   
-        
+#         print "final_state_1:",final_state[1].size()
+
         return rnn_out, final_state
- 
 
     def forward(self, decoder_input, z, drop_prob, initial_state=None):
         """
@@ -86,7 +83,6 @@ class Decoder(nn.Module):
 
         rnn_out, final_state = self.rnn(decoder_input, initial_state)
         rnn_out = rnn_out.contiguous().view(-1, self.params.decoder_rnn_size)
-        
 
         result = self.fc(rnn_out)
         result = result.view(batch_size, seq_len, self.params.word_vocab_size)
