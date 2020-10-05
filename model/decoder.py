a00b2e7c52cb49c5dc621b2c13a6910130a92f36
davidberenstein1957 <david.m.berenstein@gmail.com>
2020-10-05 07:10:26 +0000

    started implemented stacked residual lstm

--- model/decoder.py
+++ model/decoder.py
@@ -67,6 +67,88 @@ class Decoder(nn.Module):
 
         return result, final_state
 
+class ResidualDecoder(nn.Module):
+    def __init__(self, params):
+        super(ResidualDecoder, self).__init__()
+
+        self.params = params
+
+        self.rnn = nn.LSTM(input_size=self.params.latent_variable_size + self.params.word_embed_size,
+                           hidden_size=self.params.latent_variable_size + self.params.word_embed_size,
+                           num_layers=self.params.decoder_num_layers,
+                           batch_first=True,
+                           bidirectional=False)
+
+        self.rnn_2 = nn.LSTM(input_size=self.params.latent_variable_size + self.params.word_embed_size,
+                           hidden_size=self.params.decoder_rnn_size,
+                           num_layers=1,
+                           batch_first=True,
+                           bidirectional=False)
+
+        self.fc = nn.Linear(self.params.decoder_rnn_size, self.params.word_vocab_size)
+
+    def only_decoder_beam(self, decoder_input, z, drop_prob, encoder_outputs, initial_state=None):
+
+        assert parameters_allocation_check(self), \
+            'Invalid CUDA options. Parameters should be allocated in the same memory'
+        [beam_batch_size, _, _] = decoder_input.size()
+        '''
+            decoder rnn is conditioned on context via additional bias = W_cond * z to every input token
+        '''
+        decoder_input = F.dropout(decoder_input, drop_prob)
+        z = z.unsqueeze(0)
+        z = t.cat([z] * beam_batch_size, 0)
+        decoder_input = t.cat([decoder_input, z], 2)
+        rnn_out, final_state = self.rnn(decoder_input, initial_state)
+
+        return rnn_out, final_state
+
+    # https://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
+    def residual_enrolling(self, decoder_input,  initial_state):
+
+        [batch_size, seq_len, _] = decoder_input.size()
+        for sentence_id in range(batch_size):
+            for word_id in range(seq_len):
+                word = decoder_input[word_id][sentence_id][:].view(1, 1, -1)
+                state = initial_state[:, sentence_id, :]
+                _, (h_n, c_n) = self.rnn(word, state)
+                rnn_out, final_state = self.rnn_2(h_n)
+
+        return rnn_out, final_state
+    
+    def forward(self, decoder_input, z, drop_prob, encoder_outputs, initial_state=None):
+        """
+        :param decoder_input: tensor with shape of [batch_size, seq_len, embed_size]
+        :param z: sequence context with shape of [batch_size, latent_variable_size]
+        :param drop_prob: probability of an element of decoder input to be zeroed in sense of dropout
+        :param initial_state: initial state of decoder rnn
+
+        :return: unnormalized logits of sentense words distribution probabilities
+                    with shape of [batch_size, seq_len, word_vocab_size]
+                 final rnn state with shape of [num_layers, batch_size, decoder_rnn_size]
+        """
+
+        assert parameters_allocation_check(self), \
+            'Invalid CUDA options. Parameters should be allocated in the same memory'
+
+        [batch_size, seq_len, _] = decoder_input.size()
+
+        '''
+            decoder rnn is conditioned on context via additional bias = W_cond * z to every input token
+        '''
+        decoder_input = F.dropout(decoder_input, drop_prob)
+
+        z = t.cat([z] * seq_len, 1).view(batch_size, seq_len, self.params.latent_variable_size)
+        decoder_input = t.cat([decoder_input, z], 2)
+
+        rnn_out, final_state = self.residual_enrolling(decoder_input, initial_state)
+        rnn_out = rnn_out.contiguous().view(-1, self.params.decoder_rnn_size)
+
+        result = self.fc(rnn_out)
+        result = result.view(batch_size, seq_len, self.params.word_vocab_size)
+
+        return result, final_state
+
 class AttnDecoder(nn.Module):
     def __init__(self, params):
         super(AttnDecoder).__init__()
