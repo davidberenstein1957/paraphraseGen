6d7036b8e19a71a479bc99e3c10b064a7afe6118
davidberenstein1957 <david.m.berenstein@gmail.com>
2020-10-15 11:08:06 +0000

    added dropout to residual lstm

--- model/decoder.py
+++ model/decoder.py
@@ -84,13 +84,15 @@ class DecoderResidual(nn.Module):
                            hidden_size=self.params.decoder_rnn_size,
                            num_layers=self.params.decoder_num_layers,
                            batch_first=True,
-                           bidirectional=False)
+                           bidirectional=False,
+                           dropout=0.5)
 
         self.rnn_2 = nn.LSTM(input_size=self.params.decoder_rnn_size,
                            hidden_size=self.params.decoder_rnn_size,
                            num_layers=self.params.decoder_num_layers,
                            batch_first=True,
-                           bidirectional=False)
+                           bidirectional=False,
+                           dropout=0.5)
         
         # self.rnn_3 = nn.LSTM(input_size=self.params.decoder_rnn_size,
         #                    hidden_size=self.params.decoder_rnn_size,
@@ -128,13 +130,13 @@ class DecoderResidual(nn.Module):
         rnn_out_1, _ = self.rnn_1(decoder_input, initial_state)
         rnn_out = t.add(rnn_out_1, decoder_input)
         rnn_out_2, final_state = self.rnn_2(rnn_out, initial_state)    
-        # rnn_out = t.add(rnn_out_2, rnn_out)
+        rnn_out = t.add(rnn_out_2, rnn_out_1)
         # rnn_out_3, _ = self.rnn_3(rnn_out, initial_state)    
         # rnn_out = t.add(rnn_out_3, rnn_out)
         # rnn_out_4, final_state = self.rnn_4(rnn_out, initial_state)  
         # rnn_out = t.add(rnn_out_4, rnn_out)
 
-        return rnn_out_2, final_state
+        return rnn_out, final_state
     
     def forward(self, decoder_input, z, drop_prob, encoder_outputs, initial_state=None):
         """
