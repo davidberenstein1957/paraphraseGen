872a40341ee0648c5c7f91eecdab9469cbc73018
davidberenstein1957 <david.m.berenstein@gmail.com>
2020-11-30 13:22:41 +0000

    created inputless hrvae

--- test.py
+++ test.py
@@ -40,11 +40,11 @@ if __name__ == "__main__":
 
     if parser.parse_args().test_type == "final":
         parser.add_argument(
-            "--test-file", type=str, default=path + f"data/test_{parser.parse_args().data_name}.txt", metavar="NS", help="test file path (default: data/test.txt)"
+            "--test-file", type=str, default=path + f'data/test_{parser.parse_args().data_name}.txt', metavar="NS", help="test file path (default: data/test.txt)"
         )
     else:
         parser.add_argument(
-            "--test-file", type=str, default=path + f"data/test_final_{parser.parse_args().data_name}.txt", metavar="NS", help="test file path (default: data/test.txt)"
+            "--test-file", type=str, default=path + f'data/test_final_{parser.parse_args().data_name}.txt', metavar="NS", help="test file path (default: data/test.txt)"
         )
     # parser.add_argument(
     #     "--test-file", type=str, default=path + f"data/test_final_{parser.parse_args().data_name}.txt", metavar="NS", help="test file path (default: data/test.txt)"
